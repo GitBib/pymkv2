757d6b2848628dc19978e58293c82f74ff0a0c34
GitBib <15717621+GitBib@users.noreply.github.com>
2024-05-19 02:25:29 +0300

    Refactor package imports and add __all__ declaration
    The package imports in the __init__.py file of the pymkv module have been organized. Additionally, an __all__ declaration has been added, listing all the modules that should be imported when 'from pymkv import *' is used. This ensures that only these modules are exposed when the package is imported, enhancing the encapsulation and control over the module usage.

--- pymkv/__init__.py
+++ pymkv/__init__.py
@@ -1,15 +1,14 @@
 # sheldon woodward
 # august 5, 2019
 
-from pkg_resources import get_distribution, DistributionNotFound
+from pkg_resources import DistributionNotFound, get_distribution
 
 # package imports
 from .MKVAttachment import MKVAttachment
-from .MKVTrack import MKVTrack
 from .MKVFile import MKVFile
+from .MKVTrack import MKVTrack
 from .Timestamp import Timestamp
 from .Verifications import verify_matroska, verify_mkvmerge, verify_recognized, verify_supported
-
 
 # set the version number within the package using setuptools-scm
 try:
@@ -17,3 +16,14 @@ try:
 except DistributionNotFound:
     # package is not installed
     __version__ = None
+
+__all__ = (
+    "MKVAttachment",
+    "MKVFile",
+    "MKVTrack",
+    "Timestamp",
+    "verify_matroska",
+    "verify_mkvmerge",
+    "verify_recognized",
+    "verify_supported",
+)
