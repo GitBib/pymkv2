75097da6d626c556b02a9d5d095ce596f816cb5d
GitBib <15717621+GitBib@users.noreply.github.com>
2024-05-19 22:55:02 +0300

    Refactor Timestamp class and add type annotations
    The Timestamp class in the pymkv module was updated to include type annotations for improved code clarity and potential type checking benefits. The changes also included a refactor of some methods for improved readability and understanding of the code. Moreover, docstrings were added for some methods to provide additional context.

--- pymkv/Timestamp.py
+++ pymkv/Timestamp.py
@@ -1,13 +1,18 @@
-# sheldon woodward
-# 3/16/18
-
-"""Timestamp Class"""
+from __future__ import annotations
 
 from re import match
 
 
 class Timestamp:
-    def __init__(self, timestamp=None, hh=None, mm=None, ss=None, nn=None, form='MM:SS'):
+    def __init__(  # noqa: PLR0913
+        self,
+        timestamp: str | int | Timestamp = None,
+        hh: int | None = None,
+        mm: int | None = None,
+        ss: int | None = None,
+        nn: int | None = None,
+        form: str | None = "MM:SS",
+    ) -> None:
         """A class that represents a timestamp used in MKVFiles.
 
         The Timestamp class represents a timestamp used in mkvmerge. These are commonly used for splitting MKVFiles.
@@ -25,7 +30,7 @@ class Timestamp:
             Seconds in the timestamp. This will override the seconds in the given timestamp.
         nn (int):
             Nanoseconds in the timestamp. This will override the nanoseconds in the given timestamp.
-        form (int):
+        form (str):
             A str for the form of the returned timestamp. 'MM' and 'SS' must be included where 'HH' and 'NN' are
             optional but will be included if 'hh' and 'nn' are not zero.
         """
@@ -47,16 +52,43 @@ class Timestamp:
             self._ss = 0 if self._ss is None else self._ss
             self._nn = 0 if self._nn is None else self._nn
 
-    def __eq__(self, other):
+    def __eq__(self, other: Timestamp) -> bool:
+        """
+        Compares the Timestamp object with another Timestamp object for equality.
+
+        Args:
+            other: The Timestamp object to compare with.
+
+        Returns:
+            bool: True if the Timestamp objects are equal, False otherwise.
+        """
         return self.hh == other.hh and self.mm == other.mm and self.ss == other.ss and self.nn == other.nn
 
-    def __ne__(self, other):
+    def __ne__(self, other: Timestamp) -> bool:
+        """
+        Compares the Timestamp object with another Timestamp object for inequality.
+
+        Args:
+            other: The Timestamp object to compare with.
+
+        Returns:
+            bool: True if the Timestamp objects are not equal, False otherwise.
+        """
         return self.hh != other.hh or self.mm != other.mm or self.ss != other.ss or self.nn != other.nn
 
-    def __lt__(self, other):
+    def __lt__(self, other: Timestamp) -> bool:
+        """
+        Compares the Timestamp object with another Timestamp object to determine if it is less than.
+
+        Args:
+            other: The Timestamp object to compare with.
+
+        Returns:
+            bool: True if the Timestamp object is less than the other Timestamp object, False otherwise.
+        """
         if self.hh != other.hh:
             return self.hh < other.hh
-        elif self.mm != other.mm:
+        elif self.mm != other.mm:  # noqa: RET505
             return self.mm < other.mm
         elif self.ss != other.ss:
             return self.ss < other.ss
@@ -64,10 +96,19 @@ class Timestamp:
             return self.nn < other.nn
         return False
 
-    def __le__(self, other):
+    def __le__(self, other: Timestamp) -> bool:
+        """
+        Compares the Timestamp object with another Timestamp object to determine if it is less than or equal to.
+
+        Args:
+            other: The Timestamp object to compare with.
+
+        Returns:
+            bool: True if the Timestamp object is less than or equal to the other Timestamp object, False otherwise.
+        """
         if self.hh != other.hh:
             return self.hh <= other.hh
-        elif self.mm != other.mm:
+        elif self.mm != other.mm:  # noqa: RET505
             return self.mm <= other.mm
         elif self.ss != other.ss:
             return self.ss <= other.ss
@@ -75,10 +116,19 @@ class Timestamp:
             return self.nn <= other.nn
         return True
 
-    def __gt__(self, other):
+    def __gt__(self, other: Timestamp) -> bool:
+        """
+        Compares the Timestamp object with another Timestamp object to determine if it is greater than.
+
+        Args:
+            other: The Timestamp object to compare with.
+
+        Returns:
+            bool: True if the Timestamp object is greater than the other Timestamp object, False otherwise.
+        """
         if self.hh != other.hh:
             return self.hh > other.hh
-        elif self.mm != other.mm:
+        elif self.mm != other.mm:  # noqa: RET505
             return self.mm > other.mm
         elif self.ss != other.ss:
             return self.ss > other.ss
@@ -86,10 +136,19 @@ class Timestamp:
             return self.nn > other.nn
         return False
 
-    def __ge__(self, other):
+    def __ge__(self, other: Timestamp) -> bool:
+        """
+        Compares the Timestamp object with another Timestamp object to determine if it is greater than or equal to.
+
+        Args:
+            other: The Timestamp object to compare with.
+
+        Returns:
+            bool: True if the Timestamp object is greater than or equal to the other Timestamp object, False otherwise.
+        """
         if self.hh != other.hh:
             return self.hh >= other.hh
-        elif self.mm != other.mm:
+        elif self.mm != other.mm:  # noqa: RET505
             return self.mm >= other.mm
         elif self.ss != other.ss:
             return self.ss >= other.ss
@@ -97,33 +156,42 @@ class Timestamp:
             return self.nn >= other.nn
         return True
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.ts
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> int:
+        """
+        Retrieves the element at the specified index from the Timestamp object.
+
+        Args:
+            index: The index of the element to retrieve.
+
+        Returns:
+            int: The element at the specified index.
+        """
         return (self.hh, self.mm, self.ss, self.ss)[index]
 
     @property
-    def ts(self):
+    def ts(self) -> str:
         """Generates the timestamp specified in the object."""
         # parse timestamp format
-        format_groups = match('^(([Hh]{1,2}):)?([Mm]{1,2}):([Ss]{1,2})(\.([Nn]{1,9}))?$', self.form).groups()
+        format_groups = match(r"^(([Hh]{1,2}):)?([Mm]{1,2}):([Ss]{1,2})(\.([Nn]{1,9}))?$", self.form).groups()
         timestamp_format = [format_groups[i] is not None for i in (1, 2, 3, 5)]
 
         # create timestamp string
-        timestamp_string = ''
+        timestamp_string = ""
         if timestamp_format[0] or self._hh:
-            timestamp_string += '{0:0=2d}:'.format(self.hh)
+            timestamp_string += f"{self.hh:0=2d}:"
         if timestamp_format[1] or self._mm:
-            timestamp_string += '{0:0=2d}:'.format(self.mm)
+            timestamp_string += f"{self.mm:0=2d}:"
         if timestamp_format[2] or self._ss:
-            timestamp_string += '{0:0=2d}'.format(self.ss)
+            timestamp_string += f"{self.ss:0=2d}"
         if timestamp_format[3] or self._nn:
-            timestamp_string += '{:.9f}'.format(self.nn / 1000000000).rstrip('0')[1:] if self.nn else '.0'
+            timestamp_string += f"{self.nn / 1000000000:.9f}".rstrip("0")[1:] if self.nn else ".0"
         return timestamp_string
 
     @ts.setter
-    def ts(self, timestamp):
+    def ts(self, timestamp: Timestamp) -> None:
         """Set a new timestamp.
 
         timestamp (str, int):
@@ -131,7 +199,8 @@ class Timestamp:
             the basis of the timestamp.
         """
         if not isinstance(timestamp, (int, str)):
-            raise TypeError(f'"{type(timestamp)}" is not str or int type')
+            msg = f'"{type(timestamp)}" is not str or int type'
+            raise TypeError(msg)
         self._hh = None
         self._mm = None
         self._ss = None
@@ -139,59 +208,60 @@ class Timestamp:
         self.extract(timestamp)
 
     @property
-    def hh(self):
+    def hh(self) -> int:
         return self._hh
 
     @hh.setter
-    def hh(self, value):
+    def hh(self, value: int) -> None:
         self._hh = value
 
     @property
-    def mm(self):
+    def mm(self) -> int:
         return self._mm
 
     @mm.setter
-    def mm(self, value):
-        self._mm = value if value < 60 else 0
+    def mm(self, value: int) -> None:
+        self._mm = value if value < 60 else 0  # noqa: PLR2004
 
     @property
-    def ss(self):
+    def ss(self) -> int:
         return self._ss
 
     @ss.setter
-    def ss(self, value):
-        self._ss = value if value < 60 else 0
+    def ss(self, value: int) -> None:
+        self._ss = value if value < 60 else 0  # noqa: PLR2004
 
     @property
-    def nn(self):
+    def nn(self) -> int:
         return self._nn
 
     @nn.setter
-    def nn(self, value):
-        self._nn = value if value < 1000000000 else 0
+    def nn(self, value: int) -> None:
+        self._nn = value if value < 1000000000 else 0  # noqa: PLR2004
 
     @property
-    def form(self):
+    def form(self) -> str:
         return self._form
 
     @form.setter
-    def form(self, form):
+    def form(self, form: str) -> None:
         self._form = form
 
     @staticmethod
-    def verify(timestamp):
+    def verify(timestamp: str | int) -> bool:
         """Verify a timestamp has the proper form to be used in mkvmerge.
 
         timestamp (str, int):
             The timestamp to be verified.
         """
         if not isinstance(timestamp, str):
-            raise TypeError(f'"{type(timestamp)}" is not str type')
-        elif match('^[0-9]{1,2}(:[0-9]{1,2}){1,2}(\.[0-9]{1,9})?$', timestamp):
+            msg = f'"{type(timestamp)}" is not str type'
+            raise TypeError(msg)
+        elif match(r"^[0-9]{1,2}(:[0-9]{1,2}){1,2}(\.[0-9]{1,9})?$", timestamp):  # noqa: RET506
             return True
         return False
 
-    def extract(self, timestamp):
+    def extract(self, timestamp: str | int) -> None:
         """Extracts time info from a timestamp.
 
         timestamp (str, int):
@@ -199,9 +269,11 @@ class Timestamp:
             extracted from this parameter.
         """
         if not isinstance(timestamp, (str, int)):
-            raise TypeError(f'"{type(timestamp)}" is not str or int type')
-        elif isinstance(timestamp, str) and not Timestamp.verify(timestamp):
-            raise ValueError(f'"{timestamp}" is not a valid timestamp')
+            msg = f'"{type(timestamp)}" is not str or int type'
+            raise TypeError(msg)
+        elif isinstance(timestamp, str) and not Timestamp.verify(timestamp):  # noqa: RET506
+            msg = f'"{timestamp}" is not a valid timestamp'
+            raise ValueError(msg)
         elif isinstance(timestamp, str):
             self.splitting_timestamp(timestamp)
         elif isinstance(timestamp, int):
@@ -210,19 +282,14 @@ class Timestamp:
             self._ss = int(timestamp % 3600 % 60)
             self._nn = 0
 
-    def splitting_timestamp(self, timestamp):
+    def splitting_timestamp(self, timestamp: str) -> None:
         """
-        The `splitting_timestamp` function splits a timestamp into its individual components and assigns them to instance variables.
-
+        The `splitting_timestamp` function splits a timestamp into its individual
+        components and assigns them to instance variables.
         Args:
-        self: The instance of the class.
-
+            timestamp: The timestamp string to be parsed and split into components.
         Returns:
-        None
-
-        Raises:
-        None
-
+            None
         Examples:
         ```
         timestamp = "12:34:56.789"
@@ -231,15 +298,12 @@ class Timestamp:
         print(self.mm)  # Output: 34
         print(self.ss)  # Output: 56
         print(self.nn)  # Output: 789000000
-            Parameters
-            ----------
-            timestamp
-
-            Returns
-            -------
+        Parameters
+        ----------
+        timestamp
         '''
         """
-        timestamp_groups = match('^(([0-9]{1,2}):)?([0-9]{1,2}):([0-9]{1,2})(\.([0-9]{1,9}))?$', timestamp).groups()
+        timestamp_groups = match(r"^(([0-9]{1,2}):)?([0-9]{1,2}):([0-9]{1,2})(\.([0-9]{1,9}))?$", timestamp).groups()
 
         timestamp = [timestamp_groups[i] for i in (1, 2, 3, 4)]
         timestamp_clean = []
