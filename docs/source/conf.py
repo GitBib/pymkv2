24190c8a85ca69363a05539aec5732a6cb3f6cb4
GitBib <15717621+GitBib@users.noreply.github.com>
2024-06-11 18:16:57 +0300

    Update pymkv2 version to 2.0.6
    The version of pymkv2 has been updated in the pyproject.toml file and in the documentation's conf.py file. This update marks the next minor release of the pymkv2 project.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2024, Sheldon Woodward & GitBib"
 author = "Sheldon Woodward & GitBib"
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.5"
+release = "2.0.6"
 
 
 # -- General configuration ---------------------------------------------------
