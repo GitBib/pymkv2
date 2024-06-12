a047295c613419051b3791107b6e1b58817d04a0
GitBib <15717621+GitBib@users.noreply.github.com>
2024-06-12 12:01:43 +0300

    Add new test cases to test_actions_track.py
    This commit introduces multiple test cases in test_actions_track.py which cover muxing functionality, track handling, and handling of title in MKV files. Each introduced test function validates a specific boundary case or functionality and asserts the expected behavior.

--- tests/test_actions_track.py
+++ tests/test_actions_track.py
@@ -66,3 +66,102 @@ def test_move_track_front_and_mux(get_base_path: Path, get_path_test_file: Path)
     assert len(mkv.tracks) == 2  # noqa: PLR2004
     assert mkv.tracks[0].track_type == "audio"
     assert mkv.tracks[1].track_type == "video"
+
+
+def test_mux_and_test_title(get_base_path: Path, get_path_test_file: Path) -> None:
+    output_file = get_base_path / "file-test.mkv"
+
+    mkv = MKVFile(get_path_test_file)
+    mkv.title = "Test title in mkv file"
+    mkv.mux(output_file)
+
+    mkv = MKVFile(output_file)
+
+    assert mkv.title == "Test title in mkv file"
+
+
+def test_mux_and_test_track_name(get_base_path: Path, get_path_test_file: Path) -> None:
+    output_file = get_base_path / "file-test.mkv"
+
+    mkv = MKVFile(get_path_test_file)
+    mkv.tracks[0].track_name = "Test track name"
+    mkv.mux(output_file)
+
+    mkv = MKVFile(output_file)
+
+    assert mkv.tracks[0].track_name == "Test track name"
+
+
+def test_move_track_end_raises(get_path_test_file: Path) -> None:
+    mkv = MKVFile(get_path_test_file)
+
+    with pytest.raises(IndexError):
+        mkv.move_track_end(-1)
+
+    with pytest.raises(IndexError):
+        mkv.move_track_end(2)
+
+
+def test_move_track_end_and_mux(get_base_path: Path, get_path_test_file: Path) -> None:
+    output_file = get_base_path / "file-test.mkv"
+
+    mkv = MKVFile(get_path_test_file)
+    mkv.move_track_end(0)
+    mkv.mux(output_file)
+
+    mkv = MKVFile(output_file)
+
+    assert len(mkv.tracks) == 2  # noqa: PLR2004
+    assert mkv.tracks[0].track_type == "audio"
+    assert mkv.tracks[1].track_type == "video"
+
+
+def test_move_track_forward_raises(get_path_test_file: Path) -> None:
+    mkv = MKVFile(get_path_test_file)
+
+    with pytest.raises(IndexError):
+        mkv.move_track_forward(-1)
+
+    with pytest.raises(IndexError):
+        mkv.move_track_forward(2)
+
+    with pytest.raises(IndexError):
+        mkv.move_track_forward(1)
+
+
+def test_move_track_forward_and_mux(get_base_path: Path, get_path_test_file: Path) -> None:
+    output_file = get_base_path / "file-test.mkv"
+
+    mkv = MKVFile(get_path_test_file)
+    mkv.move_track_forward(0)
+    mkv.mux(output_file)
+
+    mkv = MKVFile(output_file)
+
+    assert len(mkv.tracks) == 2  # noqa: PLR2004
+    assert mkv.tracks[0].track_type == "audio"
+    assert mkv.tracks[1].track_type == "video"
+
+
+def test_move_track_backward_raises(get_path_test_file: Path) -> None:
+    mkv = MKVFile(get_path_test_file)
+
+    with pytest.raises(IndexError):
+        mkv.move_track_backward(-1)
+
+    with pytest.raises(IndexError):
+        mkv.move_track_backward(2)
+
+
+def test_move_track_backward_and_mux(get_base_path: Path, get_path_test_file: Path) -> None:
+    output_file = get_base_path / "file-test.mkv"
+
+    mkv = MKVFile(get_path_test_file)
+    mkv.move_track_backward(1)
+    mkv.mux(output_file)
+
+    mkv = MKVFile(output_file)
+
+    assert len(mkv.tracks) == 2  # noqa: PLR2004
+    assert mkv.tracks[0].track_type == "audio"
+    assert mkv.tracks[1].track_type == "video"
