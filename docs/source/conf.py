70d53552cd88596ddd01d9f7f23092a8aa054f0a
GitBib <15717621+GitBib@users.noreply.github.com>
2024-12-07 02:35:36 +0300

    Bump version to 2.1.0
    Update the project version from 2.0.10 to 2.1.0 in both the documentation and configuration files. This change likely reflects new features, enhancements, or bug fixes introduced since the previous version. Ensure to verify compatibility and functionality with this new version update.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2024, Sheldon Woodward & GitBib"  # noqa: A001
 author = "Sheldon Woodward & GitBib"
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.10"
+release = "2.1.0"
 
 
 # -- General configuration ---------------------------------------------------
